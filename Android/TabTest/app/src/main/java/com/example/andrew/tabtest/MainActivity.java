770a559a0168a5f0ceb82a6bdd428a485e1f77f9
Andrew W <andrwww@utexas.edu>
2016-10-25 15:22:28 -0500

    changes to TabTest

--- Android/TabTest/app/src/main/java/com/example/andrew/tabtest/MainActivity.java
+++ Android/TabTest/app/src/main/java/com/example/andrew/tabtest/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.andrew.tabtest;
 
+import android.support.design.widget.TabLayout;
+
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar)findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new HomeFragment(), "Hydration");
+        viewPagerAdapter.addFragments(new TopFreeFragment(), "Respiration");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    /** Called when the user clicks the Send button. Must be public and void*/
+    public void dispMessage(View view){
+
     }
 }
