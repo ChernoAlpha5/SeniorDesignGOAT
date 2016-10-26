3343bdb28cc6cb713876183d8861157df52e591c
Andrew W <andrwww@utexas.edu>
2016-10-25 21:38:52 -0500

    Added AndroidMPChart lib to folder

--- Android/TabTest/app/src/main/java/com/example/andrew/tabtest/MainActivity.java
+++ Android/TabTest/app/src/main/java/com/example/andrew/tabtest/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.andrew.tabtest;
 
 import android.support.design.widget.TabLayout;
-
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     TabLayout tabLayout;
     ViewPager viewPager;
     ViewPagerAdapter viewPagerAdapter;
+    TextView hydrationLvl;
+    ArrayList<Integer> LThydration = new ArrayList<Integer>();  //LThydration = "long-term hydration"
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +37,21 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
-    /** Called when the user clicks the Send button. Must be public and void*/
+    /** Called when the user clicks the Send button. Must be public and void
+     * Displays what the user entered into the edit_message widget*/
     public void dispMessage(View view){
-
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        try{
+            Integer intHydrateLvl = Integer.parseInt(message);
+            DateFormat df = new SimpleDateFormat("EEE, d MMM, HH:mm");
+            String mydate = df.format(Calendar.getInstance().getTime());
+            hydrationLvl = (TextView)findViewById(R.id.hydrationLvl);
+            hydrationLvl.setText(intHydrateLvl  + "% as of " + mydate);
+            LThydration.add(intHydrateLvl);
+        }
+        catch(NumberFormatException e){
+            hydrationLvl.setText("Invalid entry");
+        }
     }
 }
