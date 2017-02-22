0656694a95d6f1b32125688933ae7600291f2e18
Andrew W <andrwww@utexas.edu>
2017-02-22 15:45:10 -0600

    Added arduino basicdemo

--- Arduino/BlunoBasicDemo/BlunoBasicDemo.ino
+++ Arduino/BlunoBasicDemo/BlunoBasicDemo.ino
@@ -1,14 +1,18 @@
 void setup() {
+    DDRD = DDRD | B11111100;  //set PORTD (digital 2 - 6) to output
     Serial.begin(115200);               //initial the Serial
 }
 
 
 void loop()
 {
-    for (int i =0; i < 70; i++){
+    for (int i =100000; i < 100070; i++){
+      PORTD ^= 0x8; //toggle PD3    
+      PORTD ^= 0x8; //toggle PD3
       Serial.print(i); 
+      PORTD ^= 0x8; //toggle 
       //Serial.print("\n"); 
-      delay(150);
+      //delay(150);
     }
     //Serial.write("=======\n");
     delay(500);
